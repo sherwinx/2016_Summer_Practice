c36d382da2c4d229aa353542f5f53079bf86a31f
Sherwin <sherwinxyy@gmail.com>
2016-07-26 22:11:21 +0800

    July 26th 2016 v1

--- p_list/search_in_rotated_sorted_list.py
+++ p_list/search_in_rotated_sorted_list.py
@@ -1,20 +1,29 @@
-myList = [4,5,6,7,0,1,2]
+#jmyList = [4,5,6,7,0,1,2]
 
+#myList = [4,5,6,0,1,2,3]
+myList = [4,5,6,7,8,1,2,3]
 def searchRotated(num, myList):
     start = 0
     end = len(myList) - 1
-    while start != end:  
+    while start != end and end >= 0 and start >= 0:  
+        ##whether it's monotonically increse, no matter the whole thing is rotated or not
         mid = (start + end) / 2
         if num == myList[mid]:
             return mid
-        elif num < myList[mid] and num >= myList[start]:
-            end = mid - 1
+        if myList[mid] >= myList[start]:
+            if myList[mid] >= num and num >= myList[start]:
+                end = mid
+            else:
+                start = mid + 1
         else:
-            start = mid + 1
+             if myList[mid] < num and num <= myList[end]:
+                 start = mid + 1
+             else:
+                 end = mid
     if myList[start] == num:
         return start 
     else:
         return -1
 
-print searchRotated(0, myList)
+print searchRotated(8, myList)
 
